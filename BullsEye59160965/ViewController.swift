0c2478d83ff2911149eb4c1e464b2e4ae997070b
student <student@informaticss-Mac-mini.local>
2019-01-09 16:34:55 +0700

    Initial Commit

---
+++ BullsEye59160965/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullsEye59160965
+//
+//  Created by student on 9/1/2562 BE.
+//  Copyright © 2562 Override. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
